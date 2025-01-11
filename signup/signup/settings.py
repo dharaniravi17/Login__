c2f9c0244dbbae524f742b2d3a2c747827749fe2
dharaniravi17 <dharaniaravind2@gmail.com>
2025-01-11 13:59:36 +0530

    Backend5

--- signup/signup/settings.py
+++ signup/signup/settings.py
@@ -89,8 +89,8 @@ CORS_ALLOWED_ORIGINS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'signup',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
